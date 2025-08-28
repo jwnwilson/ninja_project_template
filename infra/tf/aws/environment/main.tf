1301b754239f7add7743e96d886680cd02bc7275
Noel Wilson <noelw@ocula.tech>
2025-08-28 19:47:45 +0100

    Updating template to simplify

--- infra/tf/aws/environment/main.tf
+++ infra/tf/aws/environment/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region = "eu-west-1"
     bucket = "hexrepo-jwn"
-    key = "{{project_slug}}_be-environment.tfstate"
+    key = "{{project_slug}}-environment.tfstate"
   }
   required_providers {
     aws = {
@@ -14,15 +14,14 @@ terraform {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-
-locals {
-  db_url = "postgresql+psycopg://postgres:{password}@${module.{{project_slug}}_be_postgres.db_instance_endpoint}/${var.project}"
-  db_ro_url = module.{{project_slug}}_be_postgres.db_instance_ro_endpoint != null ? "postgresql+psycopg://postgres:{password}@${module.{{project_slug}}_be_postgres.db_instance_ro_endpoint}/${var.project}" : "postgresql+psycopg://postgres:{password}@${module.{{project_slug}}_be_postgres.db_instance_endpoint}/${var.project}"
-}
-
 locals {
     account_id = data.aws_caller_identity.current.account_id
     region = data.aws_region.current.name
+    db_url            = "postgresql+psycopg://postgres:{password}@${module.postgres.db_instance_endpoint}/${var.project}"
+    db_ro_url         = module.postgres.db_instance_ro_endpoint != null ? "postgresql+psycopg://postgres:{password}@${module.postgres.db_instance_ro_endpoint}/${var.project}" : "postgresql+psycopg://postgres:{password}@${module.postgres.db_instance_endpoint}/${var.project}"
+    api_subdomain     = "{{project_slug}}-${terraform.workspace}"
+    api_subdomain_ecs = "{{project_slug}}-${terraform.workspace}-ecs"
+    app_url           = "https://${local.api_subdomain}.${var.domain}"
     common_env_vars = {
       ENVIRONMENT             = terraform.workspace
       PROJECT                 = var.project
@@ -56,13 +55,7 @@ data "aws_vpc" "hexrepo" {
 data "aws_ecr_repository" "ecr_repo" {
   name                 = "hexrepo-${var.project}"
 }
-
-
-
-
-
-
-module "{{project_slug}}_be_ecs_api" {
+module "be_ecs_api" {
   source             = "../../../../../../infra/tf/aws/modules/ecs"
   project            = var.project
   name               = "api"
@@ -70,8 +63,8 @@ module "{{project_slug}}_be_ecs_api" {
   aws_region         = var.aws_region
   vpc_id             = data.aws_vpc.hexrepo.id
   private_subnet_ids = data.aws_subnets.private.ids
-  security_group_ids = [module.common_postgres.db_security_group_id]
-  target_group_arn   = module.common_alb.target_group_arn
+  security_group_ids = [module.postgres.db_security_group_id]
+  target_group_arn   = module.alb.target_group_arn
   # This costs money
   container_insights = "disabled"
   min_capacity       = 0
@@ -90,47 +83,24 @@ module "{{project_slug}}_be_ecs_api" {
   task_memory   = 512
 }
 
-
-
-
-
-
-
-
-
-module "{{project_slug}}_be_api_gateway" {
-  source = "../../../../../../infra/tf/aws/modules/apigateway"
-
-  environment       = terraform.workspace
-  lambda_invoke_arn = module.{{project_slug}}_be_api.lambda_function_invoke_arn
-  lambda_name       = module.{{project_slug}}_be_api.lambda_function_name
-  domain            = var.domain
-  api_subdomain     = "{{project_slug}}_be-${terraform.workspace}"
-  project           = "{{project_slug}}_be"
-  cognito_user_pool_arn = module.common_auth.user_pool_arn
-  # Auth handled in api middleware
-  auth_enabled          = false
-}
-
-
-module "{{project_slug}}_be_postgres" {
+module "be_postgres" {
   source = "../../../../../../infra/tf/aws/modules/rds"
 
   environment       = terraform.workspace
-  project           = "{{project_slug}}_be"
+  project           = "{{project_slug}}"
   vpc_id            = data.aws_vpc.hexrepo.id
   username          = "postgres"
 }
 
 data "aws_secretsmanager_secret" "db_secret" {
-  arn = module.{{project_slug}}_be_postgres.db_password_secret_arn
+  arn = module.{{project_slug}}_postgres.db_password_secret_arn
 }
 
 
 
-module "{{project_slug}}_be_bucket" {
+module "be_bucket" {
   source = "../../../../../../infra/tf/aws/modules/s3"
 
-  project     = "{{project_slug}}_be"
-  name        = "{{project_slug}}_be"
+  project     = "{{project_slug}}"
+  name        = "{{project_slug}}"
 }
